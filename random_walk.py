77367646913bd273971008e5a590ab2d52ca06bf
Lucas Gomes <lucasfgsantos01@gmail.com>
2025-05-15 14:44:23 -0300

    Adicionando mais pontos para a plotagem

--- random_walk.py
+++ random_walk.py
@@ -12,27 +12,29 @@ class RandomWalk():
         self.x_values = [0]
         self.y_values = [0]
 
+    def get_step(self):
+        # Decide direção a ser seguida e distância a ser percorrida nessa direção
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        step = direction * distance
+        return step
+        pass
+
     def fill_walk(self):
         """Calcula todos os pontos do passeio."""
 
         # Continua dando passos até que o passeio alence o tamanho desejado
         while len(self.x_values) < self.num_points:
-            # Decide direção a ser seguida e distância a ser percorrida nessa direção
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x = self.get_step()
+            y = self.get_step()
 
             # Rejeita movimentos que não vão a lugar nenhum
-            if x_step == 0 and y_step == 0:
+            if x == 0 and y == 0:
                 continue
 
             # Calcula os próximos valores de x e de y
-            next_x = self.x_values[-1] + x_step
-            next_y = self.y_values[-1] + y_step
+            next_x = self.x_values[-1] + x
+            next_y = self.y_values[-1] + y
 
             self.x_values.append(next_x)
             self.y_values.append(next_y)
@@ -41,10 +43,14 @@ class RandomWalk():
 # Continua criando varios passeios aleatórios enquanto o programa estiver ativo
 while True:
     # Cria um passeio aleatório e plota os pontos
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
+
+    # Define o tamanho da janela de plotagem
+    plt.figure(dpi=100, figsize=(10, 6))
+
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=10)
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
@@ -52,7 +58,7 @@ while True:
     # A versão do livro deve funionar em versões antigas da lib
     # plt.axes().get_xaxis().set_visible(False)
     # plt.axes().get_yaxis().set_visible(False)
-    
+
     ax = plt.gca()
     ax.axis('off') # Retira bordas e eixos
 
