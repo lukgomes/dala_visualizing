ee21d3352d18eeb9bd6b1e66ed7504a810f19c58
Lucas Gomes <lucasfgsantos01@gmail.com>
2025-05-15 13:07:29 -0300

    plotando o inicio e fim e limpando os eixos

--- random_walk.py
+++ random_walk.py
@@ -38,8 +38,30 @@ class RandomWalk():
             self.y_values.append(next_y)
 
 
-# Cria um passeio aleatório e plota os pontos
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=1)
-plt.show()
+# Continua criando varios passeios aleatórios enquanto o programa estiver ativo
+while True:
+    # Cria um passeio aleatório e plota os pontos
+    rw = RandomWalk()
+    rw.fill_walk()
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # Remove os eixos
+    # A versão do livro deve funionar em versões antigas da lib
+    # plt.axes().get_xaxis().set_visible(False)
+    # plt.axes().get_yaxis().set_visible(False)
+    
+    ax = plt.gca()
+    ax.axis('off') # Retira bordas e eixos
+
+    #ax.get_xaxis().set_visible(False) # Retira os eixos, porem deixa as bordas
+    #ax.get_yaxis().set_visible(False)
+
+
+    plt.show()
+
+    keep_running = input("Make another walk: (y/n): ")
+    if keep_running.lower() == 'n':
+        break
