1e781486fa36dc1b40351b13308df74cb1a25fce
Lucas Gomes <lucasfgsantos01@gmail.com>
2025-05-15 10:33:54 -0300

    Gerando um gráfico linear simples

---
+++ mpl_squares.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+plt.plot(input_values, [n ** 2 for n in input_values], linewidth=5)
+
+# Define o titulo do Gráfico e nomeia os eixos
+plt.title("Square Number", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# Define o tamnho dos rótulos dasmarcações
+plt.tick_params(axis='both', labelsize=14)
+plt.show()
